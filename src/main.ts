0c122edee1a63bef4ccaaa07992eb7ab54315ba1
jordancuadrado <jac59155@gmail.com>
2024-09-30 23:21:30 -0400

    posting to google sheets

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,14 @@ interface InventoryItem {
 
 const main = async () => {
   await renderDashboardWithData();
+  openAddDialogOnClick();
+  await addItemOnClick();
 };
 
 const renderDashboardWithData = async () => {
   await fetch(`${import.meta.env.VITE_GOOGLE_SHEETS_URL}?action=getInventory&userId=1`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       const tbody = document.getElementById('tbody') as HTMLTableElement;
       tbody.innerHTML = (data as InventoryItem[])
       .map((item) => 
@@ -25,4 +26,33 @@ const renderDashboardWithData = async () => {
     });
 };
 
+const openAddDialogOnClick = () => {
+  const addButton = document.getElementById("trigger-name") as HTMLButtonElement;
+  const addDialog = document.getElementById("dialogAdd") as HTMLDialogElement;
+
+  addButton.addEventListener("click", () => {
+    addDialog.showModal();  
+  });
+ }
+
+
+ const addItemOnClick = async () => {
+  const addButton = document.getElementById("add") as HTMLButtonElement;
+
+  addButton.addEventListener("click", async () => {
+    const description = document.getElementById("name") as HTMLInputElement;
+    const date = document.getElementById("date") as HTMLInputElement;
+    const inStock = document.getElementById("in-stock") as HTMLInputElement;
+    const barCode = document.getElementById("barcode") as HTMLInputElement;
+
+    await fetch(`${import.meta.env.VITE_GOOGLE_SHEETS_URL}?action=addInventory&description=${description.value}&expirationDate=${date.value}&inStock=${inStock.checked}&barCode=${barCode.value}&userId=1`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+
+      await renderDashboardWithData();
+  
+  });
+ }
 main();
