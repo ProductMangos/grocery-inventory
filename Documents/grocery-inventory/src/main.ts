38d95fd730c0306ac02d20cbfa3bdf556dc2379f
jordancuadrado <jac59155@gmail.com>
2024-09-30 20:04:47 -0400

    base

--- Documents/grocery-inventory/src/main.ts
+++ Documents/grocery-inventory/src/main.ts
@@ -1,3 +1,18 @@
 import './style.css'
 
-document.getElementById("tbody")!.innerHTML = `${import.meta.env.VITE_GOOGLE_SHEET_URL}`;
+const main = async () => {
+  await renderDataOnDashboard();
+};
+
+const renderDataOnDashboard = async () => {
+  fetch(`${import.meta.env.VITE_GOOGLE_SHEET_URL}?action=getInventory&userId=1`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+const renderDataTable = async () => {
+
+};
+
+main();
